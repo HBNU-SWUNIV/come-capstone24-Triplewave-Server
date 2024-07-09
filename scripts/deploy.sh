2aa10c4bfaec6992f375d6aabf336a910ef97de2
suri-pu-bi <ymg12347@gmail.com>
2024-07-09 20:12:00 +0900

    chore: nginx 설정 추가 및 docker-compose 이름 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,13 +11,17 @@ if [ ! -f "$LOG_FILE" ]; then
     sudo chmod 666 "$LOG_FILE"
 fi
 
+# Nginx가 없는 경우 Nginx 서비스 시작
+if ! docker ps | grep -q nginx; then
+    sudo docker-compose up -d nginx
+fi
 
 # 환경변수 DOCKER_APP_NAME을 delivery으로 설정
 DOCKER_APP_NAME=delivery
 
 # 실행중인 blue가 있는지 확인
 # 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지 여부를 EXIST_BLUE 변수에 저장
-EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.yml ps blue | grep Up)
 
 # 배포 시작한 날짜와 시간을 기록
 echo "배포 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" |  sudo tee -a "$LOG_FILE"
@@ -29,8 +33,8 @@ if [ -z "$EXIST_BLUE" ]; then
   # 로그 파일(/home/ec2-user/deploy.log)에 "blue up - blue 배포 : port:8081"이라는 내용을 추가
   echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" | sudo tee -a "$LOG_FILE"
 
-	# docker-compose.blue.yml 파일을 사용하여 spring-blue 프로젝트의 컨테이너를 빌드하고 실행
-	sudo docker compose -p ${DOCKER_APP_NAME}-blue -f "$REPOSITORY/docker-compose.blue.yml" up -d --build
+	# docker-compose.yml 파일을 사용하여 spring-blue 프로젝트의 컨테이너를 빌드하고 실행
+	sudo docker compose -p ${DOCKER_APP_NAME}-blue -f "$REPOSITORY/docker-compose.yml" up blue -d --build
 
   # 30초 동안 대기
   sleep 30
@@ -38,8 +42,8 @@ if [ -z "$EXIST_BLUE" ]; then
   # /home/ec2-user/deploy.log: 로그 파일에 "green 중단 시작"이라는 내용을 추가
   echo "green 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" | sudo tee -a "$LOG_FILE"
 
-  # docker-compose.green.yml 파일을 사용하여 spring-green 프로젝트의 컨테이너를 중지
-  sudo docker compose -p ${DOCKER_APP_NAME}-green -f "$REPOSITORY/docker-compose.green.yml" down
+  # docker-compose.yml 파일을 사용하여 spring-green 프로젝트의 컨테이너를 중지
+  sudo docker compose -p ${DOCKER_APP_NAME}-green -f "$REPOSITORY/docker-compose.yml" down green
 
    # 사용하지 않는 이미지 삭제
   sudo docker image prune -af
@@ -50,12 +54,12 @@ if [ -z "$EXIST_BLUE" ]; then
 else
 	echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" | sudo tee -a "$LOG_FILE"
 	# -p: 프로젝트 이름 지정, -f: 다른 컴포즈 파일 지정, -d: 백그라운드에서 실행, --build: 컨테이너를 시작하기 전에 이미지를 다시 빌드
-	sudo docker compose -p ${DOCKER_APP_NAME}-green -f "$REPOSITORY/docker-compose.green.yml" up -d --build
+	sudo docker compose -p ${DOCKER_APP_NAME}-green -f "$REPOSITORY/docker-compose.yml" up green -d --build
 
   sleep 30
 
   echo "blue 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" | sudo tee -a "$LOG_FILE"
-  sudo docker compose -p ${DOCKER_APP_NAME}-blue -f "$REPOSITORY/docker-compose.blue.yml" down
+  sudo docker compose -p ${DOCKER_APP_NAME}-blue -f "$REPOSITORY/docker-compose.yml" blue down
   sudo docker image prune -af
 
   echo "blue 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" |  sudo tee -a "$LOG_FILE"
