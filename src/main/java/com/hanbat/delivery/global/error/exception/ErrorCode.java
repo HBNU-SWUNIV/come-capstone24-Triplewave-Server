190a30085df95504cd47db7be7134b1cdac52277
suri-pu-bi <ymg12347@gmail.com>
2024-08-11 14:33:50 +0900

    feat: rosbridge, member, request, location 에러 코드 추가

--- src/main/java/com/hanbat/delivery/global/error/exception/ErrorCode.java
+++ src/main/java/com/hanbat/delivery/global/error/exception/ErrorCode.java
@@ -18,7 +18,20 @@ public enum ErrorCode {
 	// Authentication
 	TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "토큰이 헤더에 없습니다."),
 	AUTH_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "시큐리티 인증 정보를 찾을 수 없습니다."),
-	TOKEN_IS_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다.");
+	TOKEN_IS_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+
+	// Member
+	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "멤버를 찾을 수 없습니다."),
+
+	// Request
+	REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "요청을 찾을 수 없습니다."),
+	RECEIVER_IS_NOT_RIGHT(HttpStatus.BAD_REQUEST, "해당 주문의 수신자가 아닙니다."),
+
+	// Location
+	LOCATION_NOT_FOUND(HttpStatus.NOT_FOUND, "위치를 찾을 수 없습니다."),
+
+	// Rosbridge
+	ROSBRIDGE_NOT_CONNECTED(HttpStatus.INTERNAL_SERVER_ERROR, "Rosbridge 연결이 실패했습니다.");
 
 	private final HttpStatus status;
 	private final String message;
