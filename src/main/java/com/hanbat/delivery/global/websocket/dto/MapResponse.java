fabcf31c23f3bd37911563595e7e1e68adf2eff5
suri-pu-bi <ymg12347@gmail.com>
2024-08-19 21:56:08 +0900

    feat: map 정보 dto에 origin jsonObject 추가

--- src/main/java/com/hanbat/delivery/global/websocket/dto/MapResponse.java
+++ src/main/java/com/hanbat/delivery/global/websocket/dto/MapResponse.java
@@ -2,6 +2,8 @@ package com.hanbat.delivery.global.websocket.dto;
 
 import java.util.List;
 
+import org.json.simple.JSONObject;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,12 +16,16 @@ import lombok.ToString;
 public class MapResponse {
 	private Long width;
 	private Long height;
+	private Double resolution;
+	private JSONObject origin;
 	private List<Integer> data;
 
 	@Builder
-	public MapResponse(Long width, Long height, List<Integer> data) {
+	public MapResponse(Long width, Long height, JSONObject origin, Double resolution, List<Integer> data) {
 		this.width = width;
 		this.height = height;
+		this.origin = origin;
+		this.resolution = resolution;
 		this.data = data;
 	}
 }
