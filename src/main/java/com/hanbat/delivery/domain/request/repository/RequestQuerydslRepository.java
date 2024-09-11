83e9f93db3fb7f67718f5dc14ab5b6634f907b73
suri-pu-bi <ymg12347@gmail.com>
2024-09-11 14:22:31 +0900

    feat: 레포지토리 배달조회 추상메서드 생성

--- src/main/java/com/hanbat/delivery/domain/request/repository/RequestQuerydslRepository.java
+++ src/main/java/com/hanbat/delivery/domain/request/repository/RequestQuerydslRepository.java
@@ -1,4 +1,10 @@
 package com.hanbat.delivery.domain.request.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.hanbat.delivery.domain.request.entity.Request;
+
 public interface RequestQuerydslRepository {
+	Optional<List<Request>> findCompletedRequestsByDateRange(String dateRange);
 }
